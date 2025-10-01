53a9026369f200ac2a12ac8c8314889ad2a8b160
TechNguyen <ndt13102003@gmail.com>
2025-10-01 20:50:42 +0700

    1

---
+++ BE/Hinet.Service/Common/TreeSelect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hinet.Service.Common
+{
+    public class TreeSelect
+    {
+        public string Value { get; set; }
+        public string Title { get; set; }
+        public List<TreeSelect>? Children { get; set; }
+    }
+}
